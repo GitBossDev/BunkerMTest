9e68c1702e4341cbd8622c85169924f4092a0a9f
Diego Ch <mdchuchon@cic.es>
2026-04-06 09:10:43 +0200

    Ultimos cambios en alerts e incorporacion de simulador

--- greenhouse-simulator/src/Greenhouse.Controller/obj/Debug/net10.0/Greenhouse.Controller.AssemblyInfo.cs
+++ greenhouse-simulator/src/Greenhouse.Controller/obj/Debug/net10.0/Greenhouse.Controller.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Greenhouse.Controller")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+317a71e0d4abf4a1ccd702035b06695d71e10436")]
 [assembly: System.Reflection.AssemblyProductAttribute("Greenhouse.Controller")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Greenhouse.Controller")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
